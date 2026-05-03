4e3d0288b085cd5cc50d2548a52892c20c43b250
Vedant shreeshail Tattimani <25u1041@students.git.edu>
2026-05-03 20:28:03 +0530

    initial clean upload to new account

---
+++ test_gemini2.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from google import genai
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+try:
+    models = client.models.list()
+    for m in models:
+        print(m.name)
+except Exception as e:
+    print("Error:", e)
